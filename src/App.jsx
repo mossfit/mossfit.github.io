a0f2206b0d867bbab9975529cd3a12f989fc0114
Mainak <108523198+mossfit@users.noreply.github.com>
2026-04-23 01:18:22 +0900

    Import CSS file and remove Tailwind directives

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-@tailwind base; @tailwind components; @tailwind utilities;
+import './index.css';
 import React, { useState, useEffect, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import {
